390134b303fe424a73e9f11ee631261f96ad6674
mohammedrezq <mohammedrezq2000@gmail.com>
2021-08-15 16:55:48 +0200

    Fix issues

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const defaultConfig = require("./node_modules/@wordpress/scripts/config/webpack.config");
+
+module.exports = {
+  ...defaultConfig,
+  module: {
+    ...defaultConfig.module,
+    rules: [
+      ...defaultConfig.module.rules
+    ],
+  },
+};
