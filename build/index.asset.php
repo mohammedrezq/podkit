fad504758d17a5514425683596c00b465abcadf0
mohammedrezq <mohammedrezq2000@gmail.com>
2021-08-25 00:12:02 +0200

    [POD-6] Make the Tabs Panel only for our custom Tab Panel Styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '007eddd8dc50731360ebc806ec0c6ba8');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd8a11ba8a8506e5f1f0bc55364740740');
