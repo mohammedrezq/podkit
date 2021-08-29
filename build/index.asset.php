d135bb30a655e4756f0d71dba0d56244bfc21ee5
mohammedrezq <mohammedrezq2000@gmail.com>
2021-08-29 21:46:24 +0200

    [POD-8] Initial Addition of Responsive Devices Preview

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fa00e7ff5cd6c8f4a29adc940d94fee2');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '48f5c622aa72e5fefb7cff80367c4ad4');
